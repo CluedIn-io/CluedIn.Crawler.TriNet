6903242f6c73cbb79974161f570763273c67476f
Tim D. Ward <tiw@cluedin.com>
2021-03-25 12:08:30 +1000

    Added VOcab Mappings

---
+++ src/Trinet.Crawling/ClueProducers/EmployeeClueProducer.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using CluedIn.Core;
+using CluedIn.Core.Data;
+using CluedIn.Crawling.Factories;
+using CluedIn.Crawling.Trinet.Core.Models;
+using CluedIn.Crawling.Trinet.Infrastructure;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using CluedIn.Crawling.Trinet.Vocabularies;
+using CluedIn.Crawling.Helpers;
+using CluedIn.Core.Data.Parts;
+using RestSharp;
+
+namespace CluedIn.Crawling.Trinet.ClueProducers
+{
+    public class EmployeeClueProducer : BaseClueProducer<Employee>
+    {
+        private readonly IClueFactory _factory;
+        private readonly ILogger<TrinetClient> _log;
+
+        public EmployeeClueProducer([NotNull] IClueFactory factory, ILogger<TrinetClient> _log)
+        {
+            _factory = factory ?? throw new ArgumentNullException(nameof(factory));
+            this._log = _log;
+        }
+
+        protected override Clue MakeClueImpl(Employee input, Guid id)
+        {
+            var clue = _factory.Create("/Employee", input.EmployeeId, id);
+            var data = clue.Data.EntityData;
+
+            var vocab = new EmployeeVocabulary();
+
+            //if (!string.IsNullOrEmpty(input.Degree))
+            //{
+            //    data.Name = input.Degree;
+            //}
+
+            if (input.Names != null)
+            {
+                if (input.Names.Any())
+                {
+                    data.Name = string.Format("{0} {1}", input.Names.First().FirstName, input.Names.First().LastName);
+                    //foreach(var name in input.Names)
+                    //{
+                    //    data.Aliases.Add(name.);
+                    //}
+                }
+            }
+
+            if (input.EmploymentInfo != null)
+            {
+                data.Properties[vocab.BusinessTitle] = input.EmploymentInfo.BusinessTitle.PrintIfAvailable();
+                data.Properties[vocab.CustomGroupA] = input.EmploymentInfo.CustomGroupA.PrintIfAvailable();
+                data.Properties[vocab.CustomGroupB] = input.EmploymentInfo.CustomGroupB.PrintIfAvailable();
+                data.Properties[vocab.Department] = input.EmploymentInfo.Department.PrintIfAvailable();
+                data.Properties[vocab.EffectiveDate] = input.EmploymentInfo.EffectiveDate.PrintIfAvailable();
+                data.Properties[vocab.EmployeeClass] = input.EmploymentInfo.EmployeeClass.PrintIfAvailable();
+                data.Properties[vocab.EmployeeType] = input.EmploymentInfo.EmployeeType.PrintIfAvailable();
+                data.Properties[vocab.EmploymentStatus] = input.EmploymentInfo.EmploymentStatus.PrintIfAvailable();
+                data.Properties[vocab.EventCode] = input.EmploymentInfo.EventCode.PrintIfAvailable();
+                data.Properties[vocab.EventDesc] = input.EmploymentInfo.EventDesc.PrintIfAvailable();
+                data.Properties[vocab.FlsaCode] = input.EmploymentInfo.FlsaCode.PrintIfAvailable();
+                data.Properties[vocab.JobCode] = input.EmploymentInfo.JobCode.PrintIfAvailable();
+                data.Properties[vocab.Location] = input.EmploymentInfo.Location.PrintIfAvailable();
+                data.Properties[vocab.PayGroup] = input.EmploymentInfo.PayGroup.PrintIfAvailable();
+
+                data.Properties[vocab.ReasonCode] = input.EmploymentInfo.ReasonCode.PrintIfAvailable();
+                data.Properties[vocab.ReasonDesc] = input.EmploymentInfo.ReasonDesc.PrintIfAvailable();
+                data.Properties[vocab.RegularTemporary] = input.EmploymentInfo.RegularTemporary.PrintIfAvailable();
+                data.Properties[vocab.ServiceDate] = input.EmploymentInfo.ServiceDate.PrintIfAvailable();
+
+                data.Properties[vocab.StandardHours] = input.EmploymentInfo.StandardHours.PrintIfAvailable();
+                data.Properties[vocab.Supervisor] = input.EmploymentInfo.Supervisor.PrintIfAvailable();
+                data.Properties[vocab.TerminationDate] = input.EmploymentInfo.TerminationDate.PrintIfAvailable();
+
+                data.Properties[vocab.WorkEmail] = input.EmploymentInfo.WorkEmail.PrintIfAvailable();
+                if (input.EmploymentInfo.WorkEmail != null)
+                {
+                    var code = new EntityCode("/Employee", "CluedIn", input.EmploymentInfo.WorkEmail);
+                    data.Codes.Add(code);
+                }
+
+                data.Properties[vocab.WorkersCompCode] = input.EmploymentInfo.WorkersCompCode.PrintIfAvailable();
+                data.Properties[vocab.WorkPhone] = input.EmploymentInfo.WorkPhone.PrintIfAvailable();
+            }
+
+            data.Properties[vocab.AlternateId] = input.AlternateId.PrintIfAvailable();
+            data.Properties[vocab.EmployeeId] = input.EmployeeId.PrintIfAvailable();
+
+            if (input.EmployeePhoto != null)
+            {
+                if (!string.IsNullOrEmpty(input.EmployeePhoto.Uri))
+                {
+                    RawDataPart rawDataPart = null;
+
+                    try
+                    {
+                        var download = new RestClient().DownloadData(new RestRequest(input.EmployeePhoto.Uri));
+
+                        rawDataPart = new RawDataPart
+                        {
+                            Type = "/RawData/PreviewImage",
+                            MimeType = input.EmployeePhoto.MimeType,
+                            FileName = input.EmployeePhoto.Uri,
+                            RawDataMD5 = FileHashUtility.GetMD5Base64String(download),
+                            RawData = Convert.ToBase64String(download)
+                        };
+
+                        if (rawDataPart != null)
+                        {
+                            clue.Details.RawData.Add(rawDataPart);
+                            clue.Data.EntityData.PreviewImage = new ImageReferencePart(rawDataPart, 255, 255);
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        _log.LogWarning(exception, "Could not download Trinet Photo Url for Employee");
+                    }
+                }
+
+                if (!data.OutgoingEdges.Any())
+                {
+                    _factory.CreateEntityRootReference(clue, EntityEdgeType.PartOf);
+                }
+
+            }
+
+            return clue;
+        }
+    }
+}
--- src/Trinet.Crawling/Vocabularies/EmployeeVocabulary.cs
+++ src/Trinet.Crawling/Vocabularies/EmployeeVocabulary.cs
@@ -17,20 +17,65 @@ namespace CluedIn.Crawling.Trinet.Vocabularies
 
             AddGroup("Trinet Details", group =>
             {
-                Degree = group.Add(new VocabularyKey("Degree", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
-                EndDate = group.Add(new VocabularyKey("EndDate", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
-                Discipline = group.Add(new VocabularyKey("Discipline", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
-                SchoolName = group.Add(new VocabularyKey("SchoolName", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
-                StartDate = group.Add(new VocabularyKey("StartDate", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                BusinessTitle = group.Add(new VocabularyKey("BusinessTitle", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                CustomGroupA = group.Add(new VocabularyKey("CustomGroupA", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                CustomGroupB = group.Add(new VocabularyKey("CustomGroupB", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                Department = group.Add(new VocabularyKey("Department", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                EffectiveDate = group.Add(new VocabularyKey("EffectiveDate", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                EmployeeClass = group.Add(new VocabularyKey("EmployeeClass", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                EmployeeType = group.Add(new VocabularyKey("EmployeeType", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                EmploymentStatus = group.Add(new VocabularyKey("EmploymentStatus", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                EventCode = group.Add(new VocabularyKey("EventCode", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                EventDesc = group.Add(new VocabularyKey("EventDesc", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                FlsaCode = group.Add(new VocabularyKey("FlsaCode", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                JobCode = group.Add(new VocabularyKey("JobCode", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                Location = group.Add(new VocabularyKey("Location", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                PayGroup = group.Add(new VocabularyKey("PayGroup", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                ReasonCode = group.Add(new VocabularyKey("ReasonCode", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                ReasonDesc = group.Add(new VocabularyKey("ReasonDesc", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                RegularTemporary = group.Add(new VocabularyKey("RegularTemporary", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                ServiceDate = group.Add(new VocabularyKey("ServiceDate", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                StandardHours = group.Add(new VocabularyKey("StandardHours", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                Supervisor = group.Add(new VocabularyKey("Supervisor", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                TerminationDate = group.Add(new VocabularyKey("TerminationDate", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                WorkEmail = group.Add(new VocabularyKey("WorkEmail", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                WorkersCompCode = group.Add(new VocabularyKey("WorkersCompCode", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                WorkPhone = group.Add(new VocabularyKey("WorkPhone", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                AlternateId = group.Add(new VocabularyKey("AlternateId", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
+                EmployeeId = group.Add(new VocabularyKey("EmployeeId", VocabularyKeyDataType.Text, VocabularyKeyVisibility.Visible));
             });
+
+            this.AddMapping(this.WorkEmail, CluedIn.Core.Data.Vocabularies.Vocabularies.CluedInUser.Email);
+            this.AddMapping(this.WorkPhone, CluedIn.Core.Data.Vocabularies.Vocabularies.CluedInUser.PhoneNumber);
 
         }
      
-        public VocabularyKey Degree { get; internal set; }
-        public VocabularyKey EndDate { get; internal set; }
-        public VocabularyKey Discipline { get; internal set; }
-        public VocabularyKey SchoolName { get; internal set; }
-        public VocabularyKey StartDate { get; internal set; }
+        public VocabularyKey BusinessTitle { get; internal set; }
+        public VocabularyKey CustomGroupA { get; internal set; }
+        public VocabularyKey CustomGroupB { get; internal set; }
+        public VocabularyKey Department { get; internal set; }
+        public VocabularyKey EffectiveDate { get; internal set; }
+        public VocabularyKey EmployeeClass { get; internal set; }
+        public VocabularyKey EmployeeType { get; internal set; }
+        public VocabularyKey EmploymentStatus { get; internal set; }
+        public VocabularyKey EventCode { get; internal set; }
+        public VocabularyKey EventDesc { get; internal set; }
+        public VocabularyKey FlsaCode { get; internal set; }
+        public VocabularyKey JobCode { get; internal set; }
+        public VocabularyKey Location { get; internal set; }
+        public VocabularyKey PayGroup { get; internal set; }
+        public VocabularyKey ReasonCode { get; internal set; }
+        public VocabularyKey ReasonDesc { get; internal set; }
+        public VocabularyKey RegularTemporary { get; internal set; }
+        public VocabularyKey ServiceDate { get; internal set; }
+        public VocabularyKey StandardHours { get; internal set; }
+        public VocabularyKey Supervisor { get; internal set; }
+        public VocabularyKey TerminationDate { get; internal set; }
+        public VocabularyKey WorkEmail { get; internal set; }
+        public VocabularyKey WorkersCompCode { get; internal set; }
+        public VocabularyKey WorkPhone { get; internal set; }
+        public VocabularyKey AlternateId { get; internal set; }
+        public VocabularyKey EmployeeId { get; internal set; }
     }
 }
 
